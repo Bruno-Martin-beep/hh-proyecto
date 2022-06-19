c6883ea4500164618362d034681ec49bf6801880
Bruno-Martin-beep <brunico040@gmail.com>
2022-06-19 18:02:20 -0300

    Style card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,27 @@
+import "./Card.scss"
 import imagen from "../assets/image.jpg";
 import iconHome from "../assets/icon-home.svg";
 import iconCard from "../assets/icon-card.svg";
 
 const Card = () => {
   return (
-    <div>
+    <div className="card">
       <img src={imagen} alt="persona sonriendo" />
-      <img src={iconHome} alt="" />
-      <div>
+      <img src={iconHome} alt="" className="card-icon-home"/>
+      <div className="card-info">
         <h3>lorem ipsum dolor</h3>
-        <p>
+        <p className="card-text">
           Quis mollis nisl nunc et massa vestibulum sed metus in lorem tristique
         </p>
-        <div>
+        <div className="card-item">
           <img src={iconCard} alt="" />
           <p>Lorem ipsum dolo sit ultrice</p>
         </div>
-        <div>
+        <div className="card-item">
           <img src={iconCard} alt="" />
           <p>Lorem ipsum dolo sit ultrice</p>
         </div>
-        <button type="button">lo quiero ya</button>
+        <button type="button" className="card-button">lo quiero ya</button>
       </div>
     </div>
   );
