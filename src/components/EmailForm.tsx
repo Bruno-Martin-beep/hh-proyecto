f117a1d4e0b5091a52cc09f5cd7a7028d7c788ed
Bruno-Martin-beep <brunico040@gmail.com>
2022-06-20 20:24:26 -0300

    Style emailform

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,7 +1,9 @@
 import React, { Dispatch, useState } from "react";
+import "./EmailForm.scss";
 import { Step } from "../App";
 import { emailRegex } from "../utils/emailRegex";
 import { postData, OptionValue } from "../utils/fetchData";
+import classNames from "classnames";
 
 interface Props {
   optionSelected: OptionValue;
@@ -35,18 +37,25 @@ const EmailForm = ({ optionSelected, setStep }: Props) => {
   };
 
   return (
-    <form onSubmit={(event) => handleSubmit(event)}>
-      <label>
-        Correo electrónico
-        <input
-          type="text"
-          value={email}
-          onChange={(event) => handleEmail(event)}
-        />
-        {showError && <p>error</p>}
-      </label>
-      <button type="submit">siguiente</button>
-    </form>
+    <>
+      <h2>Para terminar completá el siguiente formulario.</h2>
+      <form onSubmit={(event) => handleSubmit(event)} className="email-form">
+        <label className="email">
+          <p className="email-text">Correo electrónico</p>
+          <input
+            type="text"
+            placeholder="juan@example.com"
+            value={email}
+            className={classNames("email-input", {
+              invalid: showError,
+            })}
+            onChange={(event) => handleEmail(event)}
+          />
+          {showError && <p className="email-error">Por favor, ingresá un correo electrónico válido.</p>}
+        </label>
+        <button type="submit" className="submit-button">enviar</button>
+      </form>
+    </>
   );
 };
 
