27c9ed324f975daa5491b3510820d72d825dffdf
Bruno-Martin-beep <brunico040@gmail.com>
2022-06-21 18:13:25 -0300

    Refactor test EmailForm

--- src/__tests__/EmailForm.test.tsx
+++ src/__tests__/EmailForm.test.tsx
@@ -3,49 +3,70 @@ import userEvent from "@testing-library/user-event";
 import EmailForm from "../components/EmailForm";
 import { OptionValue } from "../utils/fetchData";
 
-test("Display the EmailForm component", async () => {
+test("when typing a correct email", async () => {
   const optionSelected: OptionValue = "A";
 
   render(<EmailForm optionSelected={optionSelected} setStep={() => {}} />);
 
   const input = screen.getByRole("textbox");
-  const textError = screen.queryByText(
-    "Por favor, ingresá un correo electrónico válido."
-  );
   const submit = screen.getByText("enviar");
 
   expect(input).toBeVisible();
   expect(input).not.toHaveClass("invalid");
   expect(submit).toBeVisible();
-  expect(textError).toBeNull();
-
-  userEvent.type(input, "dino");
-  userEvent.click(submit);
-
-  expect(input).toHaveClass("invalid");
-
-  const textError0 = screen.queryByText(
-    "Por favor, ingresá un correo electrónico válido."
-  );
-  expect(textError0).toBeVisible();
-
-  userEvent.clear(input);
-  userEvent.click(submit);
-
-  expect(input).toHaveClass("invalid");
-
-  const textError1 = screen.queryByText(
-    "Por favor, ingresá un correo electrónico válido."
-  );
-  expect(textError1).toBeVisible();
 
   userEvent.type(input, "dino@facha.com");
   userEvent.click(submit);
 
   expect(input).not.toHaveClass("invalid");
 
-  const textError2 = screen.queryByText(
+  const textError = screen.queryByText(
     "Por favor, ingresá un correo electrónico válido."
   );
-  expect(textError2).toBeNull();
+  expect(textError).toBeNull();
 });
+
+test("when typing a wrong email", () => {
+  const optionSelected: OptionValue = "B";
+
+  render(<EmailForm optionSelected={optionSelected} setStep={() => {}} />);
+
+  const input = screen.getByRole("textbox");
+  const submit = screen.getByText("enviar");
+
+  expect(input).toBeVisible();
+  expect(input).not.toHaveClass("invalid");
+  expect(submit).toBeVisible();
+
+  userEvent.type(input, "dino");
+  userEvent.click(submit);
+
+  expect(input).toHaveClass("invalid");
+
+  const textError = screen.queryByText(
+    "Por favor, ingresá un correo electrónico válido."
+  );
+  expect(textError).toBeVisible();
+}) 
+
+test("when typing a empty email", () => {
+  const optionSelected: OptionValue = "C";
+
+  render(<EmailForm optionSelected={optionSelected} setStep={() => {}} />);
+
+  const input = screen.getByRole("textbox");
+  const submit = screen.getByText("enviar");
+
+  expect(input).toBeVisible();
+  expect(input).not.toHaveClass("invalid");
+  expect(submit).toBeVisible();
+
+  userEvent.click(submit);
+
+  expect(input).toHaveClass("invalid");
+
+  const textError = screen.queryByText(
+    "Por favor, ingresá un correo electrónico válido."
+  );
+  expect(textError).toBeVisible();
+}) 
